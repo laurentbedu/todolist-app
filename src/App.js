c413ebab919adebd03d1990afb7873cdcd2774a2
laurentbedu <bedulaurent@gmail.com>
2022-08-25 13:36:10 +0200

    launch.json added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import { useState } from 'react';
 import './App.css';
 import Task from './components/Task/Task';
 
 function App() {
+
+  const task1 = {title:"une tache", completed:true};
+  const task2 = {title:"une autre tache", completed:false};
+  const [tasksList, setTasksList] = useState([task1, task2]);
+  const tasksListElement = tasksList.map(task => {
+    return (<Task title={task.title} completed={task.completed} />);
+  })
+
   return (
     <div className="App container">
-        <Task title="un truc"/>
+
+        <div className="tasks-container">
+          {tasksListElement}
+        </div>
+
     </div>
   );
 }
