a00a8754449917d98bd25353ff38e990b6bb5d5e
laurentbedu <bedulaurent@gmail.com>
2022-08-25 15:01:23 +0200

    addTask complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import { useState } from 'react';
-import './App.css';
-import AddTask from './components/AddTask/AddTask';
-import Task from './components/Task/Task';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask/AddTask";
+import Task from "./components/Task/Task";
 
 function App() {
-
-  const task1 = {title:"une tache", completed:true};
-  const task2 = {title:"une autre tache", completed:false};
+  const task1 = { title: "une tache", completed: true };
+  const task2 = { title: "une autre tache", completed: false };
 
   const [tasksList, setTasksList] = useState([task1, task2]);
 
-  const tasksListElement = tasksList.map((task, i) => {
-    return (<Task key={i} title={task.title} completed={task.completed} />);
-  })
+  const addTaskToList = (task) => {
+    setTasksList([...tasksList, task])
+    console.log(tasksList);
+  };
 
   return (
     <div className="App container">
-        <AddTask />
-        <hr/>
-        <div className="tasks-container">
-          {tasksListElement}
-        </div>
-
+      <AddTask addTaskToList={addTaskToList} />
+      <hr />
+      <div className="tasks-container">
+        {tasksList.map((task, i) => <Task key={i} title={task.title} completed={task.completed} />)}
+      </div>
     </div>
   );
 }
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,19 +1,36 @@
 import './addTask.css';
 import { FaCheckSquare } from "react-icons/fa";
+import { useState } from 'react';
 
 
-const AddTask = (props) => {
+const AddTask = ({addTaskToList}) => {
 
-    
+    const [taskValue, setTaskValue] = useState('');
+
+    const onTaskValueChanged = (evt) => {
+        evt.preventDefault();
+        setTaskValue(evt.target.value);
+        console.log(evt.target.value);
+    }
+
+    const handleClick = (evt) => {
+        // evt.preventDefault();
+        if(taskValue?.trim() === ''){
+            return;
+        }
+        const task = {title:taskValue, completed:false}
+        addTaskToList(task);
+        setTaskValue('');
+    }
 
     return(<>
         
             <div className="row no-gutters task-detail">
                 <div className="d-flex py-1 justify-content-between align-items-center">
                     <div className="w-75">
-                        <input className="form-control form-control-sm" type="text" value="" placeholder="Nom de la tache"/>
+                        <input className="form-control form-control-sm" type="text" value={taskValue} placeholder="Nom de la tache" onChange={onTaskValueChanged}/>
                     </div>
-                    <button type="button" className="btn btn-sm btn-outline-success d-flex">
+                    <button type="button" className="btn btn-sm btn-outline-success d-flex" onClick={handleClick}>
                         <FaCheckSquare/>
                     </button>
                 </div>
